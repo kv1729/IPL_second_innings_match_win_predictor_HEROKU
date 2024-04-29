ebc86deba878e2ee8c235c3105dd37b54c317b74
Kunal Dixit <kunal.dixit14@gmail.com>
2024-04-29 14:28:28 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+
+    return requirements
+
+setup(
+    name='IPL_MATCH_WIN_PREDICTOR',
+    version='0.0.1',
+    author='Kunal',
+    author_email='kunal.dixit14@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
